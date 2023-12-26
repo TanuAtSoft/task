dfc78352bb35e988b8722bcf2a0c0876750d5f36
TanuAtSoft <tanuja_gurung@softprodigy.com>
2023-12-26 17:17:37 +0530

    resolved react error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { Quoteblock } from './stories/Quoteblock';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <Quoteblock/>
+    <Suspense fallback ={<h1>This is just a storybook</h1>}>
+    <Quoteblock title="storybook"
+  subTitle ="can be run locally using npm run storybook"
+  variant = ""
+  hideUnderline="true"
+  compBackground="white"
+  ctaLinks= {[]}
+  description=""
+  textboxBackground/>
+    </Suspense>
   );
 }
 
